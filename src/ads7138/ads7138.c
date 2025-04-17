54db9e8652757457042382ee7f437792a7259c37
Hoog-v <hogeweyv@gmail.com>
2025-04-17 23:50:05 +0200

    clean up and add the test for the ads7138 sensor

--- src/ads7138/ads7138.c
+++ src/ads7138/ads7138.c
@@ -7,27 +7,22 @@
 
 #define HASH_ADS7138 0x157F34F4
 
-static inline uint16_t
-assemble_register (uint8_t opcode, uint8_t regAddr)
-{
-    uint16_t asmb_register = regAddr | (opcode << 8);
-    return asmb_register;
-}
-
 const manikin_sensor_reg_t init_regs[] = {
     /* All channels are configured as analog input*/
-    { ADS7138_REG_PIN_CFG | ADS7138_OPCODE_SET_BIT << 8, (0x0), MANIKIN_SENSOR_REG_TYPE_WRITE },
+    { ADS7138_REG(ADS7138_REG_PIN_CFG, ADS7138_OP_SET_BIT),
+      ADS7138_REG_ALL_PINS_ANALOG_INP_BIT,
+      MANIKIN_SENSOR_REG_TYPE_WRITE },
     /* Set cal bit */
-    { ADS7138_REG_GENERAL_CFG | ADS7138_OPCODE_SET_BIT << 8,
-      (0x02),
+    { ADS7138_REG(ADS7138_REG_GEN_CFG, ADS7138_OP_SET_BIT),
+      ADS7138_REG_CAL_BIT,
       MANIKIN_SENSOR_REG_TYPE_WRITE },
     /* Set all adc channels as inputs. enabled in scanning sequence. */
-    { ADS7138_REG_AUTO_SEQUENCE_CH_SEL | ADS7138_OPCODE_SET_BIT << 8,
-      (0xFF),
+    { ADS7138_REG(ADS7138_REG_AUTO_SEQ_CH_SEL, ADS7138_OP_SET_BIT),
+      ADS7138_REG_AUTO_SEQ_ALL_CH_SEL,
       MANIKIN_SENSOR_REG_TYPE_WRITE },
     /* Set Auto sequence mode on = 1. And 4th for sequence start. */
-    { ADS7138_REG_SEQUENCE_CFG | ADS7138_OPCODE_SET_BIT << 8,
-      (0x01),
+    { ADS7138_REG(ADS7138_REG_SEQUENCE_CFG, ADS7138_OP_SET_BIT),
+      ADS7138_REG_ENABLE_AUTO_SEQ,
       MANIKIN_SENSOR_REG_TYPE_WRITE }
 };
 
@@ -45,10 +40,10 @@ ads7138_init_sensor (manikin_sensor_ctx_t *sensor_ctx)
     manikin_status_t status = check_params(sensor_ctx);
     MANIKIN_ASSERT(HASH_ADS7138, (status == MANIKIN_STATUS_OK), status);
 
-    uint8_t data = 0;
-    data         = manikin_i2c_read_reg(sensor_ctx->i2c,
-                                sensor_ctx->i2c_addr,
-                                ADS7138_REG_SYSTEM_STATUS | ADS7138_OPCODE_SINGLE_READ << 8);
+    const uint8_t data
+        = manikin_i2c_read_reg(sensor_ctx->i2c,
+                               sensor_ctx->i2c_addr,
+                               ADS7138_REG(ADS7138_REG_SYSTEM_STATUS, ADS7138_OP_SINGLE_READ));
 
     MANIKIN_NON_CRIT_ASSERT(HASH_ADS7138,
                             BIT_IS_SET(data, ADS7138_REG_SYSTEM_STATUS_RSVD_BIT),
@@ -62,33 +57,36 @@ ads7138_init_sensor (manikin_sensor_ctx_t *sensor_ctx)
     return MANIKIN_STATUS_OK;
 }
 
+void
+convert_to_12_bit_range (uint8_t *data)
+{
+    for (uint8_t i = 0; i < 16; i++)
+    {
+        data[i] = data[i] << 4;
+        i++;
+        data[i] = data[i] >> 4;
+    }
+}
+
 manikin_status_t
 ads7138_read_sensor (manikin_sensor_ctx_t *sensor_ctx, uint8_t *read_buf)
 {
-    MANIKIN_ASSERT(HASH_ADS7138, (read_buf != NULL), 0);
+    MANIKIN_ASSERT(HASH_ADS7138, (read_buf != NULL), MANIKIN_STATUS_ERR_NULL_PARAM);
     manikin_status_t status = check_params(sensor_ctx);
     MANIKIN_ASSERT(HASH_ADS7138, (status == MANIKIN_STATUS_OK), status);
-    status
-        = manikin_i2c_write_reg(sensor_ctx->i2c,
-                                sensor_ctx->i2c_addr,
-                                assemble_register(ADS7138_OPCODE_SET_BIT, ADS7138_REG_SEQUENCE_CFG),
-                                1 << 4);
+    status = manikin_i2c_write_reg(sensor_ctx->i2c,
+                                   sensor_ctx->i2c_addr,
+                                   ADS7138_REG(ADS7138_REG_SEQUENCE_CFG, ADS7138_OP_SET_BIT),
+                                   ADS7138_REG_SEQ_START_BIT);
     MANIKIN_ASSERT(HASH_ADS7138, (status == MANIKIN_STATUS_OK), status);
     size_t bytes_read = manikin_i2c_read_bytes(
         sensor_ctx->i2c, sensor_ctx->i2c_addr, read_buf, ADS7138_READ_BUF_SIZE);
-    status = manikin_i2c_write_reg(
-        sensor_ctx->i2c,
-        sensor_ctx->i2c_addr,
-        assemble_register(ADS7138_OPCODE_CLEAR_BIT, ADS7138_REG_SEQUENCE_CFG),
-        1 << 4);
+    status = manikin_i2c_write_reg(sensor_ctx->i2c,
+                                   sensor_ctx->i2c_addr,
+                                   ADS7138_REG(ADS7138_REG_SEQUENCE_CFG, ADS7138_OP_CLEAR_BIT),
+                                   ADS7138_REG_SEQ_START_BIT);
     MANIKIN_ASSERT(HASH_ADS7138, bytes_read == ADS7138_READ_BUF_SIZE, MANIKIN_STATUS_ERR_READ_FAIL);
-    for (uint8_t i = 0; i < 16; i++)
-    {
-        read_buf[i] = read_buf[i] << 4;
-        i++;
-        read_buf[i] = read_buf[i] >> 4;
-    }
-
+    convert_to_12_bit_range(read_buf);
     return status;
 }
 
@@ -99,8 +97,8 @@ ads7138_deinit_sensor (manikin_sensor_ctx_t *sensor_ctx)
     MANIKIN_ASSERT(HASH_ADS7138, (status == MANIKIN_STATUS_OK), status);
     status = manikin_i2c_write_reg(sensor_ctx->i2c,
                                    sensor_ctx->i2c_addr,
-                                   ADS7138_REG_GENERAL_CFG | ADS7138_OPCODE_SET_BIT << 8,
-                                   (0x01));
+                                   ADS7138_REG(ADS7138_REG_GEN_CFG, ADS7138_OP_SET_BIT),
+                                   ADS7138_REG_SW_RESET_BIT);
     MANIKIN_ASSERT(
         HASH_ADS7138, (status == MANIKIN_STATUS_OK), MANIKIN_STATUS_ERR_SENSOR_DEINIT_FAIL);
     return MANIKIN_STATUS_OK;
--- tests/ads7138/ads7138_test.cpp
+++ tests/ads7138/ads7138_test.cpp
@@ -1,14 +1,14 @@
 #include "catch2/catch_all.hpp"
 #include <catch2/catch_session.hpp>
-#include "vl6180x/vl6180x.h"
+#include "ads7138/ads7138.h"
 #include "fake_i2c_functions.h"
 
+// Mocks or test doubles
 static uint8_t dummy_read_buf[4];
 uint8_t        handle = 1;
 // Fake sensor context
 manikin_sensor_ctx_t dummy_ctx;
 
-#define VL6180X_FRESH_OUT_OF_RESET 0x0016
 uint16_t cur_reg;
 
 size_t
@@ -24,23 +24,7 @@ custom_write_func (manikin_i2c_inst_t handle, uint8_t i2c_addr, const uint8_t *b
 size_t
 custom_read_func (manikin_i2c_inst_t handle, uint8_t i2c_addr, uint8_t *bytes, size_t len)
 {
-    if (len >= 1)
-    {
-        switch (cur_reg)
-        {
-            case VL6180X_FRESH_OUT_OF_RESET: {
-                cur_reg = 0;
-                /* Signal that sensor is ready */
-                bytes[0] = 1;
-                break;
-            }
-            default: {
-                cur_reg  = 0;
-                bytes[0] = 200;
-                break;
-            }
-        }
-    }
+    memset(bytes, 0xFF, len);
     return len;
 }
 
@@ -54,63 +38,64 @@ reset_mocks ()
     RESET_FAKE(i2c_hal_deinit);
 }
 
-// --- vl6180x_init_sensor ---
-TEST_CASE("vl6180x_init_sensor handles null parameter", "[vl6180x]")
+// --- ads7138_init_sensor ---
+TEST_CASE("ads7138_init_sensor handles null parameter", "[ads7138]")
 {
     reset_mocks();
-    REQUIRE(vl6180x_init_sensor(NULL) == MANIKIN_STATUS_ERR_NULL_PARAM);
+    REQUIRE(ads7138_init_sensor(NULL) == MANIKIN_STATUS_ERR_NULL_PARAM);
 }
 
-TEST_CASE("vl6180x_init_sensor succeeds with valid context", "[vl6180x]")
+TEST_CASE("ads7138_init_sensor succeeds with valid context", "[ads7138]")
 {
     reset_mocks();
     dummy_ctx.i2c                        = &handle;
     dummy_ctx.i2c_addr                   = 0x29;
     i2c_hal_write_bytes_fake.custom_fake = custom_write_func;
     i2c_hal_read_bytes_fake.custom_fake  = custom_read_func;
-    REQUIRE(vl6180x_init_sensor(&dummy_ctx) == MANIKIN_STATUS_OK);
+    REQUIRE(ads7138_init_sensor(&dummy_ctx) == MANIKIN_STATUS_OK);
 }
 
-// --- vl6180x_read_sensor ---
-TEST_CASE("vl6180x_read_sensor fails with null context", "[vl6180x]")
+// --- ads7138_read_sensor ---
+TEST_CASE("ads7138_read_sensor fails with null context", "[ads7138]")
 {
     reset_mocks();
-    REQUIRE(vl6180x_read_sensor(NULL, dummy_read_buf) == MANIKIN_STATUS_ERR_NULL_PARAM);
+    REQUIRE(ads7138_read_sensor(NULL, dummy_read_buf) == MANIKIN_STATUS_ERR_NULL_PARAM);
 }
 
-TEST_CASE("vl6180x_read_sensor fails with null read buffer", "[vl6180x]")
+TEST_CASE("ads7138_read_sensor fails with null read buffer", "[ads7138]")
 {
     reset_mocks();
     i2c_hal_write_bytes_fake.custom_fake = custom_write_func;
     i2c_hal_read_bytes_fake.custom_fake  = custom_read_func;
-    REQUIRE(vl6180x_read_sensor(&dummy_ctx, NULL) == MANIKIN_STATUS_ERR_NULL_PARAM);
+    REQUIRE(ads7138_read_sensor(&dummy_ctx, NULL) == MANIKIN_STATUS_ERR_NULL_PARAM);
 }
 
-TEST_CASE("vl6180x_read_sensor reads successfully", "[vl6180x]")
+TEST_CASE("ads7138_read_sensor reads successfully", "[ads7138]")
 {
     reset_mocks();
     dummy_ctx.i2c                        = &handle;
     dummy_ctx.i2c_addr                   = 0x29;
     i2c_hal_write_bytes_fake.custom_fake = custom_write_func;
     i2c_hal_read_bytes_fake.custom_fake  = custom_read_func;
-    uint8_t read_buf[1]                  = { 0 };
-    REQUIRE(vl6180x_read_sensor(&dummy_ctx, read_buf) == MANIKIN_STATUS_OK);
+    uint8_t read_buf[16]                 = { 0 };
+    REQUIRE(ads7138_read_sensor(&dummy_ctx, read_buf) == MANIKIN_STATUS_OK);
 }
 
-TEST_CASE("vl6180x_deinit_sensor handles null context", "[vl6180x]")
+// --- ads7138_deinit_sensor ---
+TEST_CASE("ads7138_deinit_sensor handles null context", "[ads7138]")
 {
     reset_mocks();
-    REQUIRE(vl6180x_deinit_sensor(NULL) == MANIKIN_STATUS_ERR_NULL_PARAM);
+    REQUIRE(ads7138_deinit_sensor(NULL) == MANIKIN_STATUS_ERR_NULL_PARAM);
 }
 
-TEST_CASE("vl6180x_deinit_sensor succeeds with valid context", "[vl6180x]")
+TEST_CASE("ads7138_deinit_sensor succeeds with valid context", "[ads7138]")
 {
     reset_mocks();
     dummy_ctx.i2c                        = &handle;
     dummy_ctx.i2c_addr                   = 0x29;
     i2c_hal_write_bytes_fake.custom_fake = custom_write_func;
     i2c_hal_read_bytes_fake.custom_fake  = custom_read_func;
-    REQUIRE(vl6180x_deinit_sensor(&dummy_ctx) == MANIKIN_STATUS_OK);
+    REQUIRE(ads7138_deinit_sensor(&dummy_ctx) == MANIKIN_STATUS_OK);
 }
 
 int
