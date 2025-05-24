ce024f87dbedb489754e1eab5a75c531b0d51a68
Hoog-v <hogeweyv@gmail.com>
2025-05-24 13:01:50 +0200

    also add the change for the recovery mechanism

--- src/sample_timer/sample_timer.c
+++ src/sample_timer/sample_timer.c
@@ -108,7 +108,7 @@ sample_timer_disable_i2c (manikin_sensor_ctx_t *sensor)
 static manikin_status_t
 sample_timer_recover_i2c (manikin_sensor_ctx_t *sensor)
 {
-    manikin_status_t status = MANIKIN_I2C_BUS_RECOVER();
+    manikin_status_t status = MANIKIN_I2C_BUS_RECOVER(sensor->i2c);
     const uint32_t   baud   = MANIKIN_I2C_GET_BAUD(sensor->i2c);
     status                  = MANIKIN_I2C_HAL_INIT(sensor->i2c, baud);
     return status;
